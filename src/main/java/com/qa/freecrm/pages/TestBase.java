392faa3e087422237e6bc22c9c9e9d993a2dd149
Naveen Khunteta <NaveenKhunteta@Administrators-MacBook-Pro-9.local>
2018-09-07 08:36:30 +0530

    added webdriver event listener code

--- src/main/java/com/qa/freecrm/pages/TestBase.java
+++ src/main/java/com/qa/freecrm/pages/TestBase.java
@@ -8,28 +8,41 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+import com.qa.eventlistener.WebEventListener;
 import com.qa.freecrm.util.Constants;
 
 public class TestBase {
-	
+
 	WebDriver driver = null;
 	Properties prop = null;
-	
-	//initialization method -- used to launch the browser and url:
-	public  WebDriver init(){
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/chromedriver");
+	EventFiringWebDriver eDriver;
+	WebEventListener eventListener;
+
+	// initialization method -- used to launch the browser and url:
+	public WebDriver init() {
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "/src/main/resources/chromedriver");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().pageLoadTimeout(Constants.PAGE_LOAD_TIME_OUT, TimeUnit.SECONDS);
 		driver.manage().deleteAllCookies();
-		//driver.manage().window().maximize();		
+		// driver.manage().window().maximize();
+
+		eDriver = new EventFiringWebDriver(driver);
+		// Now create object of EventListerHandler to register it with
+		// EventFiringWebDriver
+		eventListener = new WebEventListener();
+		eDriver.register(eventListener);
+		driver = eDriver;
 		return driver;
 	}
-	
-	public  Properties initProperty(){
+
+	public Properties initProperty() {
 		prop = new Properties();
 		try {
-			FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/config/config.properties");
+			FileInputStream ip = new FileInputStream(
+					System.getProperty("user.dir") + "/src/main/java/config/config.properties");
 			prop.load(ip);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -38,11 +51,5 @@ public class TestBase {
 		}
 		return prop;
 	}
-	
-	
-	
-	
-	
-	
 
 }
